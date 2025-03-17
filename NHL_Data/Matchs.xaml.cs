58ee58539ef54a95152157356839dee21bb619de
Maxime Noel <78099148+EyRuSia@users.noreply.github.com>
2025-03-17 19:38:41 -0400

    Modification Matchs

--- NHL_Data/Matchs.xaml.cs
+++ NHL_Data/Matchs.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -25,4 +28,47 @@ namespace NHL_UI
             InitializeComponent();
         }
     }
+
+    public class BoolToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (bool)value ? new SolidColorBrush(Colors.AliceBlue) : new SolidColorBrush(Colors.Transparent);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+
+    public class BoolToEllipseFillConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (bool)value ? new SolidColorBrush(Colors.Gray) : new SolidColorBrush(Colors.White);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+
+
+
+    public class TimelineViewModel : INotifyPropertyChanged
+    {
+        private DateTime _currentDate = DateTime.Now;
+
+        public ObservableCollection<int> Days { get; set; } = new ObservableCollection<int> { 15, 16, 17, 18, 19, 20, 21 };
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public bool IsToday(int day)
+        {
+            return _currentDate.Day == day;
+        }
+    }
+
 }
